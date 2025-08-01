1d2622cbe208100d9e0c0a9f79f6188315eeb0bd
SaloEater <danil.lazarev@gmail.com>
2025-07-31 21:32:05 -0500

    add endless gate

--- src/main/java/com/saloeater/gateways_to_emiloot/EmiIntegration.java
+++ src/main/java/com/saloeater/gateways_to_emiloot/EmiIntegration.java
@@ -5,11 +5,14 @@ import dev.emi.emi.api.EmiPlugin;
 import dev.emi.emi.api.EmiRegistry;
 import dev.emi.emi.api.recipe.EmiRecipeCategory;
 import dev.emi.emi.api.stack.EmiStack;
+import dev.shadowsoffire.gateways.gate.Gateway;
 import dev.shadowsoffire.gateways.gate.GatewayRegistry;
 import dev.shadowsoffire.gateways.gate.Reward;
+import dev.shadowsoffire.gateways.gate.endless.EndlessGateway;
 import dev.shadowsoffire.gateways.gate.normal.NormalGateway;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ItemStack;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -27,15 +30,7 @@ public class EmiIntegration implements EmiPlugin {
         GatewayRegistry.INSTANCE.getKeys().forEach(resourceLocation -> {
             var gate = GatewayRegistry.INSTANCE.getValue(resourceLocation);
             if (gate instanceof NormalGateway normalGateway) {
-                var gatePearl = new ItemStack(GATE_PEARL.get());
-                setGate(gatePearl, gate);
-                EmiRecipeCategory category = new GatewayDropEmiRecipeCategory(
-                    ResourceLocation.tryBuild(resourceLocation.getNamespace(), resourceLocation.getPath()),
-                    EmiStack.of(gatePearl),
-                    gatePearl
-                );
-                emiRegistry.addCategory(category);
-                categories.add(category);
+                var gatePearl = registerGatewayCategory(emiRegistry, resourceLocation, gate);
                 int categoryIndex = categories.size() - 1;
                 for (int i = 0; i < normalGateway.waves().size(); i++) {
                     var wave = normalGateway.waves().get(i);
@@ -45,8 +40,26 @@ public class EmiIntegration implements EmiPlugin {
                 }
                 if (isRewardsEmpty(normalGateway.rewards())) return;
                 emiRegistry.addRecipe(new GatewayDropEmiRecipe(gatePearl, resourceLocation, new GatewayDropRecipe(normalGateway.rewards()), categoryIndex));
+            } else if (gate instanceof EndlessGateway endlessGateway) {
+                var gatePearl = registerGatewayCategory(emiRegistry, resourceLocation, gate);
+                int categoryIndex = categories.size() - 1;
+                if (isRewardsEmpty(endlessGateway.baseWave().rewards())) return;
+                emiRegistry.addRecipe(new GatewayDropEmiRecipe(gatePearl, resourceLocation, new GatewayDropRecipe(endlessGateway.baseWave().rewards()), categoryIndex));
             }
         });
+    }
+
+    private static @NotNull ItemStack registerGatewayCategory(EmiRegistry emiRegistry, ResourceLocation resourceLocation, Gateway gate) {
+        var gatePearl = new ItemStack(GATE_PEARL.get());
+        setGate(gatePearl, gate);
+        EmiRecipeCategory category = new GatewayDropEmiRecipeCategory(
+            ResourceLocation.tryBuild(resourceLocation.getNamespace(), resourceLocation.getPath()),
+            EmiStack.of(gatePearl),
+            gatePearl
+        );
+        emiRegistry.addCategory(category);
+        categories.add(category);
+        return gatePearl;
     }
 
     private static boolean isRewardsEmpty(List<Reward> rewards) {
